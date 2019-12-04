f83741ca80b044eb54d88bb9411c2f5e7c42b5f0
Andrew Schmid <andrew@storestartup.com>
2019-12-04 08:33:40 -0500

    day4 part2

--- lib/day4.ex
+++ lib/day4.ex
@@ -7,8 +7,23 @@ defmodule Day4 do
     |> length()
   end
 
+  def part2() do
+    272_091..815_432
+    |> Enum.map(&to_string/1)
+    |> Enum.filter(&has_only_two_adjacent?/1)
+    |> Enum.filter(&all_increasing?/1)
+    |> length()
+  end
+
   def has_two_adjacent?(<<a, b, c, d, e, f>>) do
     a == b or b == c or c == d or d == e or e == f
+  end
+
+  def has_only_two_adjacent?(<<a, b, c, d, e, f>>) do
+    (a == b and b != c) or (b == c and c != d and b != a) or
+      (c == d and d != e and c != b) or
+      (d == e and e != f and d != c) or
+      (e == f and e != d)
   end
 
   def all_increasing?(s) do
